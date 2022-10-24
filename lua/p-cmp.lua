79708037541b65fd39c4ccb34b85346eaf3b8592
orange <orange@sjw-10.local>
2022-10-25 00:15:54 +0800

    fix: tab error

--- lua/p-cmp.lua
+++ lua/p-cmp.lua
@@ -2,6 +2,12 @@ local cmp = require 'cmp'
 local lspkind = require 'lspkind'
 local luasnip = require("luasnip")
 
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
