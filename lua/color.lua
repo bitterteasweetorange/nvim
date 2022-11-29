bb934cd733b1b444df04cdfacbe405b6a45ef119
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2022-11-29 19:33:02 +0800

    feat: add sign_define

--- lua/color.lua
+++ lua/color.lua
@@ -10,4 +10,35 @@ vim.api.nvim_create_autocmd('TextYankPost',{
   end,
 })
 
+
+vim.g.everforest_diagnostic_line_highlight = 1
 vim.cmd('colorscheme everforest')
+
+
+vim.fn.sign_define {
+  {
+    name = 'DiagnosticSignError',
+    text = 'E',
+    texthl = 'DiagnosticSignError',
+    linehl = 'ErrorLine',
+  },
+  {
+    name = 'DiagnosticSignWarn',
+    text = 'W',
+    texthl = 'DiagnosticSignWarn',
+    linehl = 'WarningLine',
+  },
+  {
+    name = 'DiagnosticSignInfo',
+    text = 'I',
+    texthl = 'DiagnosticSignInfo',
+    linehl = 'InfoLine',
+  },
+  {
+    name = 'DiagnosticSignHint',
+    text = 'H',
+    texthl = 'DiagnosticSignHint',
+    linehl = 'HintLine',
+  },
+}
+
