e9cd9978782ab4a412b9a797cf5461006476214b
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2022-12-09 21:24:51 +0800

    fix: remove Text kind by add filter option to sources
    https://github.com/hrsh7th/nvim-cmp/pull/1067

--- lua/p-cmp.lua
+++ lua/p-cmp.lua
@@ -18,19 +18,35 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-c>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior=cmp.ConfirmBehavior.Replace,
       select = true }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'luasnip' },
-    { name = 'path' },
-  }),
+      { name = 'vsnip' }, -- For vsnip users.
+      { name = 'luasnip' }, -- For luasnip users.
+      {
+        name = "nvim_lsp",
+        entry_filter = function(entry, ctx)
+          return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
+        end
+      },
+    }, {
+      { name = 'buffer' },
+    }),
   formatting = {
-    format = lspkind.cmp_format({ wirth_text = false, maxwidth = 50 })
+    format = function(entry, vim_item)
+      if vim.tbl_contains({ 'path' }, entry.source.name) then
+        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+        if icon then
+          vim_item.kind = icon
+          vim_item.kind_hl_group = hl_group
+          return vim_item
+        end
+      end
+      return lspkind.cmp_format({ with_text = true })(entry, vim_item)
+    end
   }
 })
 
