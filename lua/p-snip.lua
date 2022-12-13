6b1e1dcccb421dbbd5330597e840431edf937c37
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2022-12-13 23:00:40 +0800

    feat: add snippet

--- lua/p-snip.lua
+++ lua/p-snip.lua
@@ -9,6 +9,8 @@ ls.config.set_config({
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
 
 ls.add_snippets('typescriptreact', {
   s('log', {
@@ -17,5 +19,20 @@ ls.add_snippets('typescriptreact', {
     t(','),
     i(2, ''),
     t(')'),
+  }),
+  s('rc', {
+    t('export type '),
+    i(1),
+    t('Props = {}'),
+    t({ '', 'export function ' }),
+    d(2, function(args)
+      -- the returned snippetNode doesn't need a position; it's inserted
+      -- "inside" the dynamicNode.
+      return sn(nil, {
+        -- jump-indices are local to each snippetNode, so restart at 1.
+        i(1, args[1]),
+      })
+    end, { 1 }),
+    t('(){return null}'),
   }),
 })
