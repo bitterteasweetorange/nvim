92b33aedb916779da8a673608b76e8684ccd54cf
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2023-01-01 20:13:05 +0800

    feat: base

--- lua/text.lua
+++ lua/text.lua
@@ -2,5 +2,5 @@ local keymap = vim.keymap
 
 keymap.set('n', '<space>ta', '<cmd>ToggleAlternate<cr>')
 require('nvim-autopairs').setup({
-	disable_filetype = { 'TelescopePrompt', 'vim' }
+  disable_filetype = { 'TelescopePrompt', 'vim' }
 })
