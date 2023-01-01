bc1615d6897956d1fcbc2a8eec66c6872f54c86c
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2023-01-01 19:50:29 +0800

    fix: remove sideways

--- lua/other.lua
+++ lua/other.lua
@@ -3,9 +3,3 @@ require('toggleterm').setup({
   direction = 'float',
   open_mapping = [[<c-\>]]
 })
-
-local keymap = vim.keymap
-keymap.set('n', '[a', '<cmd>SidewaysLeft<cr>')
-keymap.set('n', ']a', '<cmd>SidewaysRight<cr>')
-keymap.set('n', 'ga', '<cmd>SidewaysJumpRight<cr>')
-
