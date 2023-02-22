f58d6880c5293b7cb8c157acf653f21e7d33be1c
sunlee <hildxd@outlook.com>
2023-02-22 14:31:18 +0800

    feat: add format keymap and remove null-ls prettier

--- lua/maps.lua
+++ lua/maps.lua
@@ -4,6 +4,7 @@ local keymap = vim.keymap
 keymap.set('n', '<leader>q', '<cmd>q<cr>')
 keymap.set('n', '<leader>w', '<cmd>w<cr>')
 keymap.set('n', '<leader>x', '<cmd>x<cr>')
+keymap.set('n', '<leader>df', '<cmd>lua vim.lsp.buf.formatting()<cr>')
 
 keymap.set('n', '<c-a>', 'gg<S-v>G')
 
--- lua/p-null.lua
+++ lua/p-null.lua
@@ -2,7 +2,8 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettier,
+    -- null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.formatting.eslint,
   },
 })
