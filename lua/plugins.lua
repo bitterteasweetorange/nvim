81996159004b038c672d3f457e3142bdf719607e
orange <bitter.tea.sweet.orange@gmail.com>
2023-06-25 03:38:52 +0800

    feat: sticky scroll

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,6 +41,7 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
   use 'jose-elias-alvarez/typescript.nvim'
   use 'jose-elias-alvarez/null-ls.nvim'
+  use 'nvim-treesitter/nvim-treesitter-context'
   use 'nvim-treesitter/nvim-treesitter'
   use 'windwp/nvim-ts-autotag'
   use 'p00f/nvim-ts-rainbow'
