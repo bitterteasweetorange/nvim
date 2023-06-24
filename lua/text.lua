e05bb232526562519e17b2ff6bafad36ff309026
orange <bitter.tea.sweet.orange@gmail.com>
2023-06-25 03:31:23 +0800

    feat: change keybinding of toggle alternate

--- lua/text.lua
+++ lua/text.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 
-keymap.set('n', '<space>ta', '<cmd>ToggleAlternate<cr>')
+keymap.set('n', '<leader>i', '<cmd>ToggleAlternate<cr>')
 require('nvim-autopairs').setup({
   disable_filetype = { 'TelescopePrompt', 'vim' }
 })
