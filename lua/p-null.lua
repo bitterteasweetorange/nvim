4286c7dcfa1da83ec23026871bc56456838ec4cd
orange <bitter.tea.sweet.orange@gmail.com>
2023-06-25 03:11:48 +0800

    feat: add typescript.vim to code action

--- lua/p-null.lua
+++ lua/p-null.lua
@@ -3,5 +3,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
+    require("typescript.extensions.null-ls.code-actions"),
   },
 })
--- lua/p-treesitter.lua
+++ lua/p-treesitter.lua
@@ -26,8 +26,8 @@ require 'nvim-treesitter.configs'.setup {
 
 require('template-string').setup({
   filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'python' }, -- filetypes where the plugin is active
-  jsx_brackets = true, -- must add brackets to jsx attributes
-  remove_template_string = false, -- remove backticks when there are no template string
+  jsx_brackets = true,                                                                        -- must add brackets to jsx attributes
+  remove_template_string = false,                                                             -- remove backticks when there are no template string
   restore_quotes = {
     -- quotes used when "remove_template_string" option is enabled
     normal = [[']],
