827fa58e5bc47d3575b3a1f98aad9da821f9fd14
orange <bitter.tea.sweet.orange@gmail.com>
2023-07-05 17:07:21 +0800

    feat: remove split keymap

--- lua/maps.lua
+++ lua/maps.lua
@@ -5,13 +5,11 @@ keymap.set('n', '<leader>q', '<cmd>q<cr>')
 keymap.set('n', '<leader>w', '<cmd>w<cr>')
 keymap.set('n', '<leader>x', '<cmd>x<cr>')
 
-keymap.set('n', '<c-a>', 'gg<S-v>G')
-
-keymap.set('n', '<leader>s', ':vsplit<Return><C-w>w', { silent = true })
-keymap.set('n', 'f', '<C-w>w')
+keymap.set('n', '<c-a>', 'ggVG')
 
 keymap.set('n', 'H', '^')
 keymap.set('n', 'L', '$')
 
 keymap.set('n', "j", [[v:count?'j':'gj']], { noremap = true, expr = true })
 keymap.set('n', "k", [[v:count?'k':'gk']], { noremap = true, expr = true })
+
