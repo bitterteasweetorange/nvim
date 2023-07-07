bdae2f7135c8ec581181b7df8ddc7866c730a98f
orange <bitter.tea.sweet.orange@gmail.com>
2023-07-07 11:51:37 +0800

    feat: surround

--- init.lua
+++ init.lua
@@ -27,7 +27,16 @@ require("lazy").setup({
   "windwp/nvim-autopairs",
   "mg979/vim-visual-multi",
   "gcmt/wildfire.vim",
-  "tpope/vim-surround",
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
 
   "nvim-telescope/telescope.nvim",
   "nvim-lua/plenary.nvim",
