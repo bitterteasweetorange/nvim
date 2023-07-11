30e05b67f31722629519a460fabf82a877afd66c
orange <bitter.tea.sweet.orange@gmail.com>
2023-07-11 13:35:53 +0800

    feat: show hidden files in telescope

--- lua/p-telescope.lua
+++ lua/p-telescope.lua
@@ -9,6 +9,11 @@ require('nvim-web-devicons').setup({
   default = true
 })
 require('telescope').setup {
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  },
   defaults = {
     file_ignore_patterns = { "node_modules", "yarn.lock" },
     dynamic_preview_title = true,
