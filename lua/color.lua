64619104d6f6188ba8dfc53fcfd50ad35b4305e5
orange <bitter.tea.sweet.orange@gmail.com>
2023-10-17 13:28:20 +0800

    style: options for colorscheme

--- lua/color.lua
+++ lua/color.lua
@@ -1,7 +1,19 @@
 vim.g.everforest_diagnostic_line_highlight = 1
 
+require('nightfox').setup({
+  options = {
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "italic,bold",
+    }
+  }
+})
+
 -- vim.cmd('colorscheme everforest')
 vim.cmd('colorscheme dawnfox')
+
+
 
 vim.fn.sign_define({
   {
