8de8f571be5d7d27fa8e719007d8ac33bc33b50d
orange <bitter.tea.sweet.orange@gmail.com>
2023-11-06 10:51:19 +0800

    fix: filter duplicated warnings by eslint & tsserver

--- lua/p-typescript.lua
+++ lua/p-typescript.lua
@@ -1,3 +1,7 @@
+local api = require('typescript-tools.api')
 require('typescript-tools').setup({
-  settings = {},
+  handlers = {
+    -- filter duplicated warnings by eslint & tsserver
+    ['textDocument/publishDiagnostics'] = api.filter_diagnostics({ 6133 }),
+  },
 })
