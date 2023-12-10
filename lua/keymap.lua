6d8f40e3219a129513504aef67f7fb61cd3aa7c4
orange <bitter.tea.sweet.orange@gmail.com>
2023-12-10 18:55:42 +0800

    feat: keymap for no highlight

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -11,3 +11,5 @@ keymap.set('n', '<leader>x', '<cmd>x<cr>')
 
 keymap.set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
 keymap.set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
+
+keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear search highlights' })
