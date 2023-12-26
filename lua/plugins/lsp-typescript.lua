3cd4e1226230b6e81e870814d003618513a03bdd
orange <bitter.tea.sweet.orange@gmail.com>
2023-12-26 19:30:48 +0800

    feat: remove keymap of imports

--- lua/plugins/lsp-typescript.lua
+++ lua/plugins/lsp-typescript.lua
@@ -2,10 +2,6 @@ return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   config = function()
-    local keymap = vim.keymap
-    keymap.set('n', '<leader>m', '<cmd>TSToolsOrganizeImports<cr>')
-    keymap.set('n', '<leader>a', '<cmd>TSToolsAddMissingImports<cr>')
-
     local api = require 'typescript-tools.api'
     require('typescript-tools').setup {
       handlers = {
